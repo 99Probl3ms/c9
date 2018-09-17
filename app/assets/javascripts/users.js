8cf6d138de222af694b8a550768590eb4ade26bb
sam scam <gansbourg@gmail.com>
2018-09-17 01:55:21 +0000

    js submission and erroe handling

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,21 +9,51 @@ $(document).on('turbolinks:load', function() {
   // prevent default submission behavior
   submitBtn.click(function(event){
     event.preventDefault()
+    submitBtn.val('Processing').prop('disabled',true);
   
+   
   // collect credit card fields
   var ccNum = $('card_number').val(),
       cvcNum = $('card_code').val(),
       expMonth = $('card_month'),
       expYear = $('card_year');
   // send to stripe
-  Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponseHandler);
+   var error = false;
+  // card validation
+    if (!Stripe.card.validateCardNumber(ccNum)){
+      error = true;
+      alert('THe credit card number appears to be invalid')
+    }
+    
+    if (!Stripe.card.validateCVC(cvcNum)){
+      error = true;
+      alert('The CVC appears to be invalid')
+    }
+    
+    if (!Stripe.card.validateExpiry(expMonth, expYear)){
+      error = true;
+      alert('The expiration date appears to be invalid')
+    }
+    
+    if (error){
+      // dont send to stripe.
+      submitBtn.prop('disabled', false).val("Sign Up");
+    } else {
+        Stripe.createToken({
+            number: ccNum,
+            cvc: cvcNum,
+            exp_month: expMonth,
+            exp_year: expYear
+          }, stripeResponseHandler);
+          return false;
+    }
   });
   // return token
-  // inject token to hidden field
+  function stripeResponseHandler(status, response){
+    var token = response.id;
+     // inject token to hidden field
+     theForm.append($('input tpye="hidden" name="user[stripe_card_token]">').val(token));
+  }
   // submit form to app
+  theForm.get(0).submit();
 });
